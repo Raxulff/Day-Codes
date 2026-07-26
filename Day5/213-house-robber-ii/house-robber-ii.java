65e66cd2a9b8a274a09bc271d7c7c5418191711c
S.Rahul Raj <24ee1453@stjosephstechnology.ac.in>
2026-07-27 00:20:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (61.28%) - LeetSync

--- Day5/213-house-robber-ii/house-robber-ii.java
+++ Day5/213-house-robber-ii/house-robber-ii.java
@@ -4,26 +4,27 @@ class Solution {
         if (n == 1)
             return nums[0];
         
-        int prev2 = nums[0];
-        int prev1 = Math.max(nums[0],nums[1]);
-        int curr = 0;
+        Integer[] dp1 = new Integer[n];
+        Integer[] dp2 = new Integer[n];
+
+        dp1[0] = nums[0];
+        dp1[1] = Math.max(nums[0],nums[1]);
+        if(n == 2)
+            return Math.max(dp1[0],dp1[1]);
         int max = Integer.MIN_VALUE;
         for(int ind = 2;ind < n-1;ind++){
-            curr = Math.max(prev1,nums[ind]+prev2);
-            prev2 = prev1;
-            prev1 = curr;
+            dp1[ind] = Math.max(dp1[ind-1],nums[ind]+dp1[ind-2]);
         }
-        max = Math.max(max,prev1);
+        max = Math.max(max,dp1[n-2]);
 
         if(n >= 3){
-            prev2 = nums[1];
-            prev1 = Math.max(nums[1],nums[2]);
+            dp2[1] = nums[1];
+            dp2[2] = Math.max(nums[1],nums[2]);
             for(int ind = 3;ind < n;ind++){
-                curr = Math.max(prev1,nums[ind]+prev2);
-                prev2 = prev1;
-                prev1 = curr;
+                dp2[ind] = Math.max(dp2[ind-1],nums[ind]+dp2[ind-2]);
             }
+            return max = Math.max(max,dp2[n-1]);
         }
-        return max = Math.max(max,prev1);
+        return max;
     }
 }
