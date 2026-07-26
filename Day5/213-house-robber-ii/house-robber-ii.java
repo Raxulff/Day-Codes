6e88285a478bcd93d741530001df7226cebf12cc
S.Rahul Raj <24ee1453@stjosephstechnology.ac.in>
2026-07-27 00:22:30 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (42.16%) - LeetSync

--- Day5/213-house-robber-ii/house-robber-ii.java
+++ Day5/213-house-robber-ii/house-robber-ii.java
@@ -10,7 +10,7 @@ class Solution {
         dp1[0] = nums[0];
         dp1[1] = Math.max(nums[0],nums[1]);
         if(n == 2)
-            return Math.max(dp1[0],dp1[1]);
+            return dp1[1];
         int max = Integer.MIN_VALUE;
         for(int ind = 2;ind < n-1;ind++){
             dp1[ind] = Math.max(dp1[ind-1],nums[ind]+dp1[ind-2]);
