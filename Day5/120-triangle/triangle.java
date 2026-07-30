b7e5f345c1e25b3c56a4465a83cb99eb84f12f6c
S.Rahul Raj <24ee1453@stjosephstechnology.ac.in>
2026-07-30 22:59:57 +0530

    Time: 1 ms (99.82%) | Memory: 46.3 MB (23.84%) - LeetSync

--- Day5/120-triangle/triangle.java
+++ Day5/120-triangle/triangle.java
@@ -1,17 +1,19 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        Integer[][] dp = new Integer[n][n];
-        return function(0,0,triangle,dp);
+        Integer[][] dp = new Integer[triangle.size()][triangle.size()];
+        return solve(triangle,0,0,dp);
     }
-    public static int function(int row,int col,List<List<Integer>> triangle,Integer[][] dp){
-        if(row == triangle.size()){
+    public static int solve(List<List<Integer>> tri,int row,int col,Integer[][] dp){
+
+        if(row == tri.size())
             return 0;
-        }
-        if(dp[row][col] != null) return dp[row][col];
-        int st = function(row+1,col,triangle,dp);
-        int rn = function(row+1,col+1,triangle,dp);
-        return dp[row][col]=triangle.get(row).get(col) + 
-        Math.min(st,rn);
+
+        if(dp[row][col] != null)
+            return dp[row][col];
+
+        int down = solve(tri,row+1,col,dp);
+        int downRight = solve(tri,row+1,col+1,dp);
+        return dp[row][col] = tri.get(row).get(col) + Math.min(down,downRight);
+        
     }
 }
