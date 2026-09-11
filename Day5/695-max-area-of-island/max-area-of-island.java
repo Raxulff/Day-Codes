96d2591aff02c7355153739b9f941821f3617837
RahulRaj S <24ee1453@stjosephstechnology.ac.in>
2026-09-11 08:55:55 +0530

    Time: 1 ms (100.00%) | Memory: 46.2 MB (95.85%) - LeetSync

--- Day5/695-max-area-of-island/max-area-of-island.java
+++ Day5/695-max-area-of-island/max-area-of-island.java
@@ -1,29 +1,24 @@
 class Solution {
-    //static int max = 0;
+    static int max = 0;
     public int maxAreaOfIsland(int[][] grid) {
-        int max = 0;
+        //int max = 0;
         int R = grid.length;
         int C = grid[0].length;
-
+    int t=0;
         for(int row = 0;row < R;row++){
             for(int col = 0;col < C;col++){
                 if(grid[row][col] == 1){
-                    max = Math.max(max,dfs(row,col,grid,R,C,0));
+                    t=Math.max(t,dfs(row,col,grid,R,C,1));
                 }
             }
         }
-        int ans = max;
-        max = 0;
-        return ans;
+       
+        return t;
     }
     public static int dfs(int row,int col,int[][] grid,int R,int C,int count){
-        if(row < 0 || row >= R || col < 0 || col >= C || grid[row][col] != 1) return 0;
-        grid[row][col] = 2;
-        return 1 + 
-        dfs(row+1,col,grid,R,C,count) +
-        dfs(row-1,col,grid,R,C,count)+
-        dfs(row,col+1,grid,R,C,count)+
-        dfs(row,col-1,grid,R,C,count);
+        if(row < 0 || row == R || col < 0 || col == C || grid[row][col] != 1) return 0;
+        grid[row][col]=2;
+        return 1 + dfs(row+1,col,grid,R,C,count)+dfs(row-1,col,grid,R,C,count)+dfs(row,col+1,grid,R,C,count)+dfs(row,col-1,grid,R,C,count);
         
     }
 }
